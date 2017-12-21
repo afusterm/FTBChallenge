d3799007454e240ca17cd0ef433fa108eceb8b53
Alejandro <afusmir@gmail.com>
2017-12-21 23:16:24 +0100

    initial commit

---
+++ FTBChallenge/Controllers/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  FTBChallenge
+//
+//  Created by Alejandro on 21/12/2017.
+//  Copyright © 2017 Alejandro. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func goToRoot(_ sender: Any) {
+        let rootVC = RootViewController()
+        self.navigationController?.pushViewController(rootVC, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
