656db14d1d6455a0bde246d74058eee2d2301150
Alejandro <afusmir@gmail.com>
2017-12-31 20:25:12 +0100

    animations in RootViewController

--- FTBChallenge/Controllers/RootViewController.swift
+++ FTBChallenge/Controllers/RootViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class RootViewController: UIViewController {
+    @IBOutlet weak var myAccountButton: UIButton!
+    
+    var logoImage: UIImageView!
+    var focusLeft: UIImageView!
+    var focusRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImage(named: "background_login.jpg")
@@ -16,11 +22,21 @@ class RootViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        startAnimation()
+        startLightFocusAnimation()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        setupLogoImage()
+        setupFocusLeftImage()
+        setupFocusRightImage()
     }
     
     // MARK: Actions
@@ -65,5 +81,91 @@ class RootViewController: UIViewController {
                   message: "No se pudieron descargar jugadores. Vuelva a intentarlo más tarde")
         
         return false
+    }
+    
+    // MARK: Properties
+    
+    let focusXMultiplier = CGFloat(0.18)
+    let focusWidthMultiplier = CGFloat(0.33)
+    let focusHeightMultiplier = CGFloat(0.2)
+    
+    var focusLeftInitialPoint: CGPoint {
+        return CGPoint(x: view.bounds.width * (-focusXMultiplier), y: 90)
+    }
+    
+    var focusLeftFinalPoint: CGPoint {
+        return CGPoint(x: view.bounds.width * focusXMultiplier, y: 90)
+    }
+    
+    var focusRightInitialPoint: CGPoint {
+        return CGPoint(x: view.bounds.width + view.bounds.width * focusXMultiplier, y: 90)
+    }
+    
+    var focusRightFinalPoint: CGPoint {
+        return CGPoint(x: view.bounds.width + view.bounds.width * (-focusXMultiplier), y: 90)
+    }
+    
+    var focusSize: CGSize {
+        return CGSize(width: view.bounds.width * (-focusWidthMultiplier), height: view.bounds.height * focusHeightMultiplier)
+    }
+    
+    // MARK: Animations
+    
+    private func setupLogoImage() {
+        let initialPoint = CGPoint(x: myAccountButton.center.x, y: -220)
+        let size = CGSize(width: view.bounds.width * 0.5, height: view.bounds.height * 0.4)
+        let image = UIImage(named: "logo_ftb.png")
+        
+        logoImage = UIImageView(image: image)
+        logoImage.contentMode = .scaleAspectFill
+        logoImage.bounds = CGRect(origin: initialPoint, size: size)
+        logoImage.center = initialPoint
+        view.addSubview(logoImage)
+    }
+    
+    private func setupFocusLeftImage() {
+        let leftImage = UIImage(named: "lateral_izq.png")
+        focusLeft =  UIImageView(image: leftImage)
+        focusLeft.contentMode = .scaleAspectFill
+        focusLeft.bounds = CGRect(origin: focusLeftInitialPoint, size: focusSize)
+        focusLeft.center = focusLeftInitialPoint
+        view.addSubview(focusLeft)
+    }
+    
+    private func setupFocusRightImage() {
+        let rightImage = UIImage(named: "lateral_der.png")
+        focusRight =  UIImageView(image: rightImage)
+        focusRight.contentMode = .scaleAspectFill
+        focusRight.bounds = CGRect(origin: focusRightInitialPoint, size: focusSize)
+        focusRight.center = focusRightInitialPoint
+        view.addSubview(focusRight)
+    }
+ 
+    private func startAnimation() {
+        let finalY = CGFloat(myAccountButton.center.y - logoImage.bounds.height / 2 - myAccountButton.bounds.height)
+        let initialPoint = CGPoint(x: myAccountButton.center.x, y: -220)
+        let finalPoint = CGPoint(x: myAccountButton.center.x, y: finalY)
+        
+        UIImageView.animate(withDuration: 1.5,
+                            animations: {
+                                self.logoImage.center = finalPoint
+        }) { (finished) in
+            if (finished) {
+                self.logoImage.center = initialPoint
+                self.startLightFocusAnimation()
+                self.startAnimation()
+            }
+        }
+    }
+    
+    private func startLightFocusAnimation() {
+        UIImageView.animate(withDuration: 1.5,
+                            animations: {
+                                self.focusLeft.center = self.focusLeftFinalPoint
+                                self.focusRight.center = self.focusRightFinalPoint
+        }) { (_) in
+            self.focusLeft.center = self.focusLeftInitialPoint
+            self.focusRight.center = self.focusRightInitialPoint
+        }
     }
 }
